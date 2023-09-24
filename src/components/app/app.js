c75375e0b18aab410d976f836e04f20012f10f53
Slava Kulikov <cornelita@yandex.ru>
2023-09-24 14:37:25 +0300

    add SearchPanel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,6 @@
 import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+
 
 import "./app.css";
 
@@ -6,6 +8,10 @@ function App() {
     return (
         <div className="app">
             <AppInfo/>
+
+            <div className="search-panel">
+        <SearchPanel/>
+            </div>
         </div>
     );
 }
