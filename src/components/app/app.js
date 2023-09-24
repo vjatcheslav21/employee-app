4f12ada69a6e3caab4949431b481923d5d50c9e0
Slava Kulikov <cornelita@yandex.ru>
2023-09-24 16:22:04 +0300

    Added EmployeesAddForm & .css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import Employeeslist from "../employees-list/employees-list";
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 
 import "./app.css";
@@ -12,10 +13,12 @@ function App() {
             <AppInfo/>
 
             <div className="search-panel">
-        <SearchPanel/>
-        <AppFilter/>
+                <SearchPanel/>
+                <AppFilter/>
             </div>
-        <Employeeslist/>
+
+                <Employeeslist/>
+                <EmployeesAddForm/>
         </div>
     );
 }
