d6029b9991344c8f018cbe88b5660619865103d0
Slava Kulikov <cornelita@yandex.ru>
2023-09-24 13:31:12 +0300

    Connecting AppInfo to app.js file

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,11 @@
+import AppInfo from "../app-info/app-info";
+
 import "./app.css";
 
 function App() {
     return (
         <div className="app">
-
+            <AppInfo/>
         </div>
     );
 }
