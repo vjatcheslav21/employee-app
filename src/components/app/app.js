4985bd50dd59266976efda315e7e667232bd7e51
Slava Kulikov <cornelita@yandex.ru>
2023-09-24 14:48:31 +0300

    Added AppFilter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,6 @@
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
 
 
 import "./app.css";
@@ -11,6 +12,7 @@ function App() {
 
             <div className="search-panel">
         <SearchPanel/>
+        <AppFilter/>
             </div>
         </div>
     );
